e37ba8826f82d608fadb987aee3d8d1487ddc38d
Rex Recio <rexrecio@gmail.com>
2021-09-20 02:41:29 +0000

    Initial commit

---
+++ simple.go
@@ -0,0 +1,22 @@
+package go_simple_module
+
+import (
+    "fmt"
+    "log"
+
+    "github.com/sugarme/tokenizer/pretrained"
+)
+
+// Preprocess returns preprocessed text
+func Preprocess() []string {
+    tk := pretrained.BertBaseUncased()
+
+    sentence := `The quick brown fox jumps over the [L A Z Y D O G]`
+    en, err := tk.EncodeSingle(sentence)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("tokens: %q\n", en.Tokens)
+    return en.Tokens
+}
